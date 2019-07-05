c575ea9ca6e404583bca766f57c2d4e3169a081d
Alira Coffman <40637263+Alira-Coffman@users.noreply.github.com>
2019-07-05 13:01:44 -0700

    Add files via upload

---
+++ dailycoding1.cpp
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <iomanip> 
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <algorithm>   
+using namespace std;
+
+
+void addToK(int arr[20], int size, int k);
+int main()
+{
+    string line;
+    int arr[20];
+    int k, i =0;
+    //prompt user for list of numbers
+    cout << "Please enter a list of numbers: ";
+    //read in list of numbers
+    getline(cin, line);
+    stringstream ss(line);
+    //prompt for k
+    cout << "Please enter a value to check: ";
+    //read in k
+    cin >> k;
+
+    while(ss.good())
+    {
+        ss >> arr[i];
+        i++;
+    }
+   //any of the numbers add to k?
+    addToK(arr, i,k);
+    return 0;   
+
+}
+void addToK(int arr[20], int size, int k)
+{
+    int temp;
+    for(int i=0; i < size; i++)
+    {
+        //find value looking for
+        int temp = k  - arr[i];
+        //does it exist in the array
+        for(int k =i+1; k < size; k++)
+        {
+            if(arr[k] == temp)
+                cout << "Values in list that add to " << k << " are " << arr[i] << " and " << arr[k]<<endl;
+        }
+
+    }
+    
+  
+}
